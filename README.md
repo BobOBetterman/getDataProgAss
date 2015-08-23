7a53c44e1be66d5fce00f821ba7c2f2866171ddc
Andrew L. Hill <AndrewLawrenceHill@gmail.com>
2015-08-23 00:41:25 -0700

    Readme Completed

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-The programming course project for Getting Data.
+# Analysis of the UCI Data Set for the Getting Data Coursera Class Project
+
+This document describes how the accompanying code, `run_analysis.R`, takes the dataset given on the course project page and converts it into the tidy data set titled `tidyData.txt`.
+
+## Code Documentation
+
+1. The first thing the code does is load in each of the relevant parts of the data. It creates objects for the sets of measurements (`X_test` and `X_train`), as well as the identifying information for each set (`y_test`, `y_train`, and `subject_test`). It also creates an object for `features`, as this is necessary to label the measurements. After the measurement data is read in, each of the columns are labeled using the names from the `features` file.
+
+2. The objects for the test set and train set each have to be combined into data sets describing which measurements belong to which subjects. These two data frames are then combined into one large data frame.
+
+3. All the columns that don't identify the subject, measure the mean, or the standard deviation are then deleted from the data frame.
+
+4. The current data frame describes each of the possible activies by a number, rather than a name. By reading in the activity labels from `activity_labels`, these values can be replaced with human readable values describing the actions taken.
+
+5. Finally, the data is melted and recast to give us the means of each of the measurements for each possible combiniation of **Subject.ID** and **Activity.Labels**.
--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,27 @@
+#Read in the test data, making it available for the program to manipulate.
 testData <- read.table("UCI HAR Dataset/test/X_test.txt")
+
+#Read in the labels to be applied to each column of data
 featuresLab <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
 
+#The labels have an index column that needs to be dropped
 featuresLab <- featuresLab[ , -1]
 
+#Name each of the columns of the measurements with their names, taken from the features file
 names(testData) <- featuresLab
 
+#These are the activity labels for the test data
 testActLab <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "Activity Labels")
 
+#These are the test subject IDs. Another column is added to keep track of whether the subject was from the test
+#or train data sets
 testSubID <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "Subject ID")
 testSubID$Test.Subject <- TRUE
 
+#Combine all the test data into one data set
 testDataSet <- cbind(testSubID, testActLab, testData)
 
+#Repeat all the above steps with the training data
 trainData <- read.table("UCI HAR Dataset/train/X_train.txt")
 names(trainData) <- featuresLab
 
@@ -22,28 +32,39 @@ trainSubID$Test.Subject <- FALSE
 
 trainDataSet <- cbind(trainSubID, trainActLab, trainData)
 
+#Combining the train and test data sets
 fullData <- rbind(trainDataSet, testDataSet)
 
+#Finding logical vectors for each column indicating whether they are mean or std measurements
 titleMean <- grepl("mean", names(fullData), ignore.case = TRUE)
 titleSTD <- grepl("std", names(fullData), ignore.case = TRUE)
 
+#Combine the mean and std logical vectors to get one logical vector indicating whether a column is mean or std (TRUE) or neither (FALSE)
 keepCols <- as.logical(titleMean + titleSTD)
+#Make sure we keep the first three data columns, as they indicate whom the measurements are for
 keepCols[1:3] <- TRUE
 
+#Subset the data to get only the measurements with mean or std
 truncDataSet <- fullData[ , keepCols]
 
+#Figure out which columns are not "meanFreq", as these are not measurements of the "mean", so we can drop them from our data set
 titleNotMeanFreq <- grep("meanFreq", names(truncDataSet), ignore.case = TRUE, invert = TRUE)
 
 truncDataSet <- truncDataSet[ , titleNotMeanFreq]
 
+#Read in the codes for the activity labels
 actLab <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
 
+#Replace the numbers in "Activity.Labels" with the human readable values describing each action
 for(i in seq_along(truncDataSet$Activity.Labels)) {
     truncDataSet$Activity.Labels[i] <- actLab[actLab[1] == truncDataSet$Activity.Labels[i], 2]
 }
 
+#Drop the unnecessary column I added in the beginning indicating whether subjects were from the train or test data set
 truncDataSet <- truncDataSet[ , -2]
 
+#Melt the data, making all the measurements variables of the "Subject.ID" and "Activity.Labels" columns
 meltData <- melt(truncDataSet, id.vars = c("Subject.ID", "Activity.Labels"))
 
+#Finally, cast the data into a tidy data frame where the mean of each variable is calculated for each combination of "Subject.ID" and "Activity.Labels"
 tidyData <- dcast(meltData, Subject.ID + Activity.Labels ~ variable, mean)
